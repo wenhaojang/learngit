29920a8163909ad4aa5aeb746c92c7251094d60e
wenhaojiang <1751434397@qq.com>
2025-07-03 14:13:23 +0800

    merge遗留问题

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 print("Hello Git!")
 print("useless things")
 print("this is a new feature branch")
-<<<<<<< HEAD
-
 print("this is a master branch")
-=======
 print("this is a new feature branch 2")
->>>>>>> devfeature1
+
